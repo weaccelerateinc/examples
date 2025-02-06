ab8b6dfb253871e29f14ee442024bd499744873d
Paul Sauer <paulrsauer@gmail.com>
2025-02-06 17:23:39 -0500

    could be right

---
+++ stripe-demo/app/api/confirm-braintree/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest } from "next/server";
+import braintree from "braintree";
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANTID!,
+  publicKey: process.env.BRAINTREE_PUBLICKEY!,
+  privateKey: process.env.BRAINTREE_PRIVATEKEY!,
+});
+
+export async function POST(request: NextRequest) {
+  const data = (await request.json()) as {
+    paymentIntentId: string;
+    checkoutId: string;
+  };
+
+  // VALIDATE CART
+
+  const result = await gateway.transaction.sale({
+    amount: "10.00",
+    paymentMethodNonce: data.paymentIntentId,
+    options: {
+      submitForSettlement: true,
+    },
+  });
+
+  return Response.json({ status: result.transaction.status, token: result.transaction.id });
+}
